c709c3ac478a6fd40a1387ca939e66ca0d0948bd
Sebastian Frizzo <sebastian.frizzo@my.jcu.edu.au>
2019-09-11 15:15:48 +1000

    Provide commit before playing with classes

---
+++ prac_06/languages.py
@@ -0,0 +1,21 @@
+from programming_language import ProgrammingLanguage
+
+
+def main():
+	languages = []
+	ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+	python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+	visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+	languages.append(ruby)
+	languages.append(python)
+	languages.append(visual_basic)
+
+	print(language for language in languages)
+
+	print("{}\n{}\n{}".format(ruby, python, visual_basic))
+
+	dynamic_languages = [language for language in languages if language.is_dynamic()]
+	print([language for language in dynamic_languages])
+
+
+main()
