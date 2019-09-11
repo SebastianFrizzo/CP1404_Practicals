9ce1c472d6431f56ccb42e311bc3b791148a8064
Sebastian Frizzo <sebastian.frizzo@my.jcu.edu.au>
2019-09-11 15:48:26 +1000

    Clean up languages for pull request

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -2,15 +2,10 @@ from programming_language import ProgrammingLanguage
 
 
 def main():
-	languages = []
 	ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
 	python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 	visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-	languages.append(ruby)
-	languages.append(python)
-	languages.append(visual_basic)
-
-	print(language for language in languages)
+	languages = [ruby, python, visual_basic]
 
 	print("{}\n{}\n{}".format(ruby, python, visual_basic))
 
