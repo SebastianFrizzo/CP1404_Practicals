993d16c57c1f0a679d0aea3f80ce71f37c5dab7a
SebastianFrizzo <sebastianfrizzolaloli@gmail.com>
2019-09-25 13:29:00 +1000

    Provide alpha Kivy app

---
+++ prac_08/KivyDemos-master/do_from_scratch_mi_to_km.py
@@ -0,0 +1,26 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+
+class ConvertMilesToKilometres(App):
+    def build(self):
+        Window.size = (900, 400)
+        self.title = 'Convert Miles to Kilometres'
+        self.root = Builder.load_file('do_from_scratch_mi_to_km.kv')
+        return self.root
+
+    def handle_convert_number(self, value):
+        result = value * 1.60934
+        self.root.ids.output_label.text = str(result)
+
+    def handle_increase_counter(self, value):
+        value += 1
+        self.root.ids.input_number.text = value
+
+    def handle_decrease_counter(self, value):
+        value += -1
+        self.root.ids.input_number.text = value
+
+
+ConvertMilesToKilometres().run()
