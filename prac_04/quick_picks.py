8ba1ae92edcc473a9cffd7df926ed523264ae4a7
SebastianFrizzo <sebastianfrizzolaloli@gmail.com>
2019-09-25 12:35:11 +1000

    Initial commit

--- prac_04/quick_picks.py
+++
@@ -1,8 +0,0 @@
-""" Asks user for number of 'quick picks' and then generates that many lines of output."""
-import random
-
-
-def main():
-	number_of_quick_picks = int(input("How many quick picks?: "))
-	for :
-		output_list.append(range(1, 46))
