a12147ab4b1b0f0ad757ab2ca3765fd8603c0002
SebastianFrizzo <sebastianfrizzolaloli@gmail.com>
2019-09-25 15:05:19 +1000

    Complete stage 2, experiment with size and layout

--- prac_08/KivyDemos-master/do_from_scratch_mi_to_km.py
+++ prac_08/KivyDemos-master/do_from_scratch_mi_to_km.py
@@ -3,23 +3,35 @@ from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.app import StringProperty
 
+MI_TO_KM_COEFFICIENT = 1.60934
+# KI_TO_MI_COEFFICIENT =
+
 
 class ConvertMilesToKilometres(App):
-    result = StringProperty()
+	result = StringProperty()
 
-    def build(self):
-        Window.size = (900, 400)
-        self.title = 'Convert Miles to Kilometres'
-        self.root = Builder.load_file('do_from_scratch_mi_to_km.kv')
-        return self.root
+	def build(self):
+		Window.size = (700, 300)
+		self.title = 'Convert Miles to Kilometres'
+		self.root = Builder.load_file('do_from_scratch_mi_to_km.kv')
+		return self.root
 
-    def handle_convert_number(self, value):
-        self.result = str(value * 1.60934)
-        self.root.ids.output_label.text = str(self.result)
+	def handle_convert_number(self, value):
+		validated_value = self.value_validation(value)
+		self.result = str(validated_value * MI_TO_KM_COEFFICIENT)
+		self.root.ids.output_label.text = str(self.result)
 
-    def handle_increment(self, value, increment_value):
-        value += increment_value
-        self.root.ids.input_number.text = str(value)
+	def handle_increment(self, value, increment_value):
+		validated_value = self.value_validation(value)
+		validated_value += increment_value
+		self.root.ids.input_number.text = str(validated_value)
+
+	def value_validation(self, value):
+		try:
+			value = float(value)
+		except ValueError:
+			value = 0.0
+		return value
 
 
 ConvertMilesToKilometres().run()
